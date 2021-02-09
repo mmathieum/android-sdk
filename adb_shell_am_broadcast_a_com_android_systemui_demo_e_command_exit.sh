9f744b3628ba6e1f637e2eeec75d7628961c9f39
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-09 09:27:20 -0500

    add adb shell commands for demo mode #screenshot

---
+++ adb_shell_am_broadcast_a_com_android_systemui_demo_e_command_exit.sh
@@ -0,0 +1,1 @@
+$ANDROID_HOME/platform-tools/adb shell am broadcast -a com.android.systemui.demo -e command exit
---
+++ adb_shell_settings_put_global_sysui_demo_allowed_1.sh
@@ -0,0 +1,1 @@
+$ANDROID_HOME/platform-tools/adb shell settings put global sysui_demo_allowed 1
